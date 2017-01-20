fabae961a8cbf696c83ffce4ea5e68016cf114c5
rich <rich.canadajr@gmail.com>
2017-01-20 17:52:32 -0600

    Adding form php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    if(isset($_POST["submit"]))
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $myEmail = "rich.canadajr@gmail.com";
+        $from = "Portfolio Form";
+        $subject = "Portfolio Message";
+        $body = "Message received from:\n$name\n$email\n\n$message";
+        if(mail($myEmail, $subject, $message, $from))
+        {
+
+        }
+        else
+        {
+            echo("Failed to send mail");
+        }
+    }
+?>
