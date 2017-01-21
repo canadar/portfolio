305c6f484b6f6a45892eb5ee9a32062f4cfe0e35
rich <rich.canadajr@gmail.com>
2017-01-20 21:53:42 -0600

    Adding use of SendGrid for Azure

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
     if(isset($_POST["submit"]))
     {
+        $curl = curl_init();
         $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
-        $myEmail = "rich.canadajr@gmail.com";
         $from = "Portfolio Form";
         $subject = "Portfolio Message";
         $body = "Message received from:\n$name\n$email\n\n$message";
-        if(mail($myEmail, $subject, $message, $from))
-        {
 
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => "{\n  \"personalizations\": [\n    {\n      \"to\": [\n        {\n          \"email\": \"rich.canadajr@gmail.com\"\n        }\n      ],\n      \"subject\": \"New Contact\"\n    }\n  ],\n  \"from\": {\n    \"email\": \"[FROM EMAIL]\"\n  },\n  \"content\": [\n    {\n      \"type\": \"text/html\",\n      \"value\": \"$name<br>$email<br>$subject<br>$message\"\n    }\n  ]\n}",
+        CURLOPT_HTTPHEADER => array(
+            "authorization: Bearer ",
+            "cache-control: no-cache",
+            "content-type: application/json"
+        ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        header('Location: index.html');
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        echo $response;
         }
-        else
-        {
-            echo("Failed to send mail mail function failed");
-        }
-    }
-    else{
-        echo("Failed to send mail: submit not set");
+
     }
 ?>
